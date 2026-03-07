bb7b3b8bdeae9269a2ede7a58b7f258214be21cb
Gayasuddin <gyasuansari145@gmail.com>
2026-03-07 19:36:41 +0530

    fix vite render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,12 +9,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:5000",
-        changeOrigin: true,
+        changeOrigin: true
       }
     }
-  },
-  preview: {
-    host: true,
-    allowedHosts: "all"
   }
 });
