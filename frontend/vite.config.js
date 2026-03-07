7a0734814640077529ca917a0f8ed1ffbaa26aef
Gayasuddin <gyasuansari145@gmail.com>
2026-03-07 11:11:06 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",   // important for Render
+    host: "0.0.0.0",
     port: 5173,
     proxy: {
       "/api": {
@@ -13,4 +13,8 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: ["ai-systems-assignment-fu6h.onrender.com"]
+  }
 });
